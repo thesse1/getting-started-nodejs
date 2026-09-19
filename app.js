4725b4be535f329fa29b4a67066b521a80bd70ef
Thomas Hesse <hesse.thomas@gmx.de>
2026-09-19 17:09:51 +0200

    Switch to Mumbai

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var cities = {cities:["Amsterdam","Berlin","New York","San Francisco","Tokyo", "Bangalore"]}
+var cities = {cities:["Amsterdam","Berlin","New York","San Francisco","Tokyo", "Mumbai"]}
 var text1 = makeid(40);
 var text2 = text1+ " "+makeid(40);
 var text3 = text2+ " "+makeid(40);
